d894172fdf710c19bed9f742152e4aed0d7ab917
xrezut <31830718+xrezut@users.noreply.github.com>
2020-05-21 15:46:49 +0100

    Added GUI and Support for MP3

--- yt-dl.py
+++ yt-dl.py
@@ -1,23 +1,71 @@
 from __future__ import unicode_literals
 import youtube_dl
+import PySimpleGUI as sg
 
 # Downloads a YT Video in MP4 Format
-def dl_MP4_single(url, playlist):
+def dl_MP4(url, playlist):
     ydl_opts = {
         'noplaylist': playlist,
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
+# Downloads a YT Video in MP3 Format
+def dl_MP3(url, playlist):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'noplaylist': playlist,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-print("Input YouTube URL to download: ")
-url = input()
+def check_if_url_supported(url):
+    extractors = youtube_dl.extractor.gen_extractors()
+    for e in extractors:
+        if e.suitable(url) and e.IE_NAME != 'generic':
+            return True
+    return False
 
-print("Download entire playlist? ")
-playlistChoice = input().lower()
-yes = {'yes','y', 'ye', ''}
-no = {'no','n'}
-if playlistChoice in yes:
-    dl_MP4_single(url, False)
-elif playlistChoice in no:
-    dl_MP4_single(url, True)
+sg.theme('DarkBlue9')	# Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('YouTube Video Downloader')],
+            [sg.Text('Enter URL of the video'), sg.InputText()],
+            [sg.Checkbox('Download Playlist', default=False)],
+            [sg.Checkbox('MP4', default=False)],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('YouTube Video Downloader', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event in (None, 'Cancel'):	# if user closes window or clicks cancel
+        break
+
+    if check_if_url_supported(values[0]) == False:
+        sg.popup('Invalid URL entered')
+    else:
+        if values[2] == True: #MP4 selected
+            if values[1] == True:
+                dl_MP4(values[0], False)
+                sg.popup('Download Successful')
+            elif values[1] == False:
+                dl_MP4(values[0], True)
+                sg.popup('Download Successful')
+        elif values[2] == False: #MP3 Selected
+            if values[1] == True:
+                dl_MP3(values[0], False)
+                sg.popup('Download Successful')
+            elif values[1] == False:
+                dl_MP3(values[0], True)
+                sg.popup('Download Successful')
+
+
+
+
+window.close()
